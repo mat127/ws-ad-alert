a91c3cca44cf465c703f0ca0c30318126b81aa1a
Petr Matoušek <petr.matousek@comprimato.com>
2022-08-28 00:03:03 +0200

    recognize new ads, last seen stored in DynamoDB

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import { parseAds } from '../libs/adParser.js';
+import { getLastAd, saveLastAd } from '../libs/ddbAdRepository.js';
+
+async function getNewAds(ads) {
+    const lastAd = await getLastAd();
+    const idx = ads.indexOf(lastAd);
+    return ads.slice(0, idx);
+}
+
+parseAds()
+    .then(ads => getNewAds(ads))
+    .then(function(newAds) {
+        // TODO: notify new ads
+        console.log(newAds);
+        if(newAds.length > 0) {
+            saveLastAd(newAds[0]);
+        }
+    })
+    .catch(error => console.log(error));

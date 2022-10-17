fdc5386b576d0327cf4ceeb00c16d3d5bb51390e
Petr Matoušek <petr.matousek@comprimato.com>
2022-10-17 23:48:13 +0200

    fix lambda packaging and deployment

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-import wscz from "./libs/site/wscz.js";
-import bazos from "./libs/site/bazos.js";
+import * as lambda from "./lambda.js";
 
-// export for AWS as a lambda handler
-export async function run() {
-    try {
-        return Promise.all(
-            [wscz, bazos]
-                .map(s => s.process())
-                .concat()
-        );
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
-
-run()
+lambda.handler()
     .then(() => process.exit());
